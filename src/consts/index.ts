3ea31629bc5e1a2a8e4764aae1e310192105855a
Ahmed Bahajjaj <Ahmed_Bahajjaj@u.nus.edu>
2021-01-29 20:08:45 +0800

    Add API key

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -2,6 +2,6 @@ export default {
   SHOPEE_URL: 'https://shopee.sg',
   API_URL: 'https://api.hackathon2021.shopee.sg/',
   // Remember to set your Shopee Hackathon Tokens
-  HACKATHON_TOKEN: '',
+  HACKATHON_TOKEN: 'Mjqosnox5n088sJQ2RUS',
   HACKATHON_USER_TOKEN: '',
 };
