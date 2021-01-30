09c17632122e9b7fca51ecd07a34b7e182502cc6
Ahmed Bahajjaj <Ahmed_Bahajjaj@u.nus.edu>
2021-01-30 10:47:29 +0800

    Add URL to mocked APIs

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -11,8 +11,8 @@ export default {
   SHOP_GET_INFO: 'https://api.hackathon2021.shopee.sg/shop/get_info',
   SHOP_GET_ITEMS: 'https://api.hackathon2021.shopee.sg/shop/get_items',
   // Mocked API Endpoints
-  USER_GET_PURCHASES: 'user/get_purchases',
-  USER_DATE_JOINED: 'user/date_joined',
-  USER_VIEW_HISTORY: 'user/view_history',
-  SHOP_GET_SALES: 'shop/get_sales'
+  USER_GET_PURCHASES: 'https://5e8bcd4f-0100-456e-b59f-a464e1d9c1f8.mock.pstmn.io/user/get_purchases',
+  USER_DATE_JOINED: 'https://5e8bcd4f-0100-456e-b59f-a464e1d9c1f8.mock.pstmn.io/user/date_joined',
+  USER_VIEW_HISTORY: 'https://5e8bcd4f-0100-456e-b59f-a464e1d9c1f8.mock.pstmn.io/user/view_history',
+  SHOP_GET_SALES: 'https://5e8bcd4f-0100-456e-b59f-a464e1d9c1f8.mock.pstmn.io/shop/get_sales'
 };
