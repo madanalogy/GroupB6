3f2fb5959c587151300f33422864bd3d0aedddd8
Ahmed Bahajjaj <Ahmed_Bahajjaj@u.nus.edu>
2021-01-30 10:05:59 +0800

    Add endpoints as constants

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -4,4 +4,15 @@ export default {
   // Remember to set your Shopee Hackathon Tokens
   HACKATHON_TOKEN: 'Mjqosnox5n088sJQ2RUS',
   HACKATHON_USER_TOKEN: '',
+  // Shopee API Endpoints
+  USER_GET_INFO: 'https://api.hackathon2021.shopee.sg/user/get_info',
+  USER_GENERATE_TOKEN: 'https://api.hackathon2021.shopee.sg/user/generate_token',
+  ITEM_GET_INFO: 'https://api.hackathon2021.shopee.sg/item/get_info',
+  SHOP_GET_INFO: 'https://api.hackathon2021.shopee.sg/shop/get_info',
+  SHOP_GET_ITEMS: 'https://api.hackathon2021.shopee.sg/shop/get_items',
+  // Mocked API Endpoints
+  USER_GET_PURCHASES: 'user/get_purchases',
+  USER_DATE_JOINED: 'user/date_joined',
+  USER_VIEW_HISTORY: 'user/view_history',
+  SHOP_GET_SALES: 'shop/get_sales'
 };
