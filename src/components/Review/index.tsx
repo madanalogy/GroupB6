223b2c6135565b518110a8b945e811a3e6b62c69
Ahmed Bahajjaj <Ahmed_Bahajjaj@u.nus.edu>
2021-01-30 11:53:23 +0800

    Add login check to Review

--- src/components/Review/index.tsx
+++ src/components/Review/index.tsx
@@ -1,13 +1,48 @@
 import * as React from 'react';
 
+import Bridge from 'libraries/bridges';
+import Button from './components/Button';
+import { getWebUrl } from 'libraries/utils/url';
+
 import './style.scss';
 
 function Review() {
-  return (
-    <>
-      <div>Hello World!</div>
-    </>
-  );
+
+  /*
+  const [isUserLoggedIn, setIsUserLoggedIn] = React.useState(false);
+
+  Bridge.isUserLoggedIn().then(({ isLoggedIn }: { isLoggedIn: boolean }) => {
+    setIsUserLoggedIn(isLoggedIn);
+  });
+
+  Bridge.loginIfNeeded(getWebUrl(`${window.location.origin}/login`));
+  */
+  // TODO: Uncomment auth check above and remove static boolean assignment
+  const isUserLoggedIn = true;
+
+  if (isUserLoggedIn) {
+    return (
+      <>
+        <div>Hello World!</div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div>Login is required!</div>
+        <br />
+        <div>
+          <Button
+            onClick={() => {
+              Bridge.loginIfNeeded(getWebUrl(`${window.location.origin}/login`));
+            }}
+          >
+            Log In
+          </Button>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Review;
